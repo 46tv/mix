c0b993eaf7a35f1bf483e169ad11f5bd41d76b03
46tv <iptv@web.de>
2026-05-14 18:29:46 +0200

    Update Linkal.php

--- PHP/Linkal.php
+++ PHP/Linkal.php
@@ -1,1 +1,49 @@
+<?php
 
+// Hedef site
+
+$url = "https://example.com";
+
+// cURL ile sayfayı çek
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+
+$html = curl_exec($ch);
+
+if(curl_errno($ch)) {
+
+    die("cURL Hatası: " . curl_error($ch));
+
+}
+
+curl_close($ch);
+
+// m3u8 linklerini bul
+
+preg_match_all('/https?:\/\/[^"\']+\.m3u8[^"\']*/i', $html, $matches);
+
+if (!empty($matches[0])) {
+
+    echo "<h3>Bulunan m3u8 linkleri:</h3>";
+
+    foreach ($matches[0] as $link) {
+
+        echo $link . "<br><br>";
+
+    }
+
+} else {
+
+    echo "m3u8 linki bulunamadı.";
+
+}
+
+?>
